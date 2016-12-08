95accbfeca3f0534f4bb3052f7fae55663882985
Simone Cirillo <simone.cirillo@horus.technology>
2016-12-08 20:32:33 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Pyblur
+##Python image blurring routines.
+Pyblur is a collection of simple image blurring routines.
+
+It supports Gaussian, Disk, Box, and Linear Motion Blur Kernels.
+
+Usage is very simple
---
+++ pyblur/__init__.py
@@ -0,0 +1,11 @@
+from .BoxBlur import BoxBlur, BoxBlur_random
+from .DefocusBlur import DefocusBlur, DefocusBlur_random
+from .GaussianBlur import GaussianBlur, GaussianBlur_random
+from .LinearMotionBlur import LinearMotionBlur, LinearMotionBlur_random
+from .RandomizedBlur import RandomizedBlur
+
+__all__ = ["BoxBlur", "BoxBlur_random", 
+           "DefocusBlur", "DefocusBlur_random",
+           "GaussianBlur", "GaussianBlur_random",
+           "LinearMotionBlur", "LinearMotionBlur_random",
+           "RandomizedBlur"]
